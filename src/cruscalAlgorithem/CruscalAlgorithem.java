90bbcb890e5212102b061e2c114e82b8e00a1916
GlenRapaj <46382374+GlenRapaj@users.noreply.github.com>
2021-10-01 12:32:08 +0200

    Clean-code

--- src/cruscalAlgorithem/CruscalAlgorithem.java
+++ src/cruscalAlgorithem/CruscalAlgorithem.java
@@ -2,90 +2,57 @@ package cruscalAlgorithem;
 
 public class CruscalAlgorithem {
 
-		public static boolean BFS( double[][] grafi, int b ) {  // , boolean[] visited
-		
-		boolean lidhur = true;
-		
-		LinkedList<Integer > que = new LinkedList<Integer >();
-		 boolean[] visited = new boolean[grafi.length ];
-		
-		
-		for ( int i = 0; i < visited.length ; i ++) { 
-			visited[i ] = false ;
-		}
-		
-		visited[b] = true ;
-		
-		removeQue( visited,  que,  grafi, b );
-		
-		while(  que.size() != 0 ) {
-			
-			// int t = it.next();
-			int t = que.poll();
-			visited[t] = true ;
-			removeQue( visited, que, grafi, t) ;
-			
-		}
-		
-		for(int j = 0 ; j < visited.length ; j++ ) {
-			
-			if( !visited[j] ) {
-				lidhur = false;
-				break ;
+		public static boolean BFS( double[][] grafi, int b ) { 
+			boolean lidhur = true;
+			LinkedList<Integer > que = new LinkedList<Integer >();
+			boolean[] visited = new boolean[grafi.length ];
+			for ( int i = 0; i < visited.length ; i ++) {  visited[i ] = false; }
+			visited[b] = true ;
+			removeQue( visited,  que,  grafi, b );
+			while(  que.size() != 0 ) {
+				int t = que.poll();
+				visited[t] = true;
+				removeQue( visited, que, grafi, t);	
 			}
-			
-		}
-		
-		return lidhur ;
+			for(int j = 0 ; j < visited.length ; j++ ) {
+				if( !visited[j] ) {
+					lidhur = false;
+					break ;
+				}	
+			}
+			return lidhur ;
 	}
 		
 	
 	public static void main(String[] args) {
-		
 		double[][] Grafi = new double[4][4];
 		
 		Grafi[ 0 ][ 0 ] = 0 ;
 		Grafi[ 0 ][ 1 ] = 1 ;
 		Grafi[ 0 ][ 2 ] = 1 ;
 		Grafi[ 0 ][ 3 ] = 4 ;
-		
 		Grafi[ 1 ][ 0 ] = 1 ;
 		Grafi[ 1 ][ 1 ] = 0 ;
 		Grafi[ 1 ][ 2 ] = 2 ;
 		Grafi[ 1 ][ 3 ] = 3 ;
-		
 		Grafi[ 2 ][ 0 ] = 1 ;
 		Grafi[ 2 ][ 1 ] = 2 ;
 		Grafi[ 2 ][ 2 ] = 0 ;
 		Grafi[ 2 ][ 3 ] = 0 ;
-		
 		Grafi[ 3 ][ 0 ] = 4 ;
 		Grafi[ 3 ][ 1 ] = 3 ;
 		Grafi[ 3 ][ 2 ] = 0 ;
 		Grafi[ 3 ][ 3 ] = 0 ;
 		
 		double[][] druPerfshires = new double[4][4];
-		// System.out.println( " Permasat e grafit G jane : " + Grafi.length );
-		
 		Cruscal cruscalAlg = new Cruscal();
-	
-		// boolean lidhshmeria = cruscalAlg.Kontrolllidhur(a, b, Grafi);
 		boolean lidhshmeria = BFS(Grafi, 0 );
 		
-		if( lidhshmeria ){
-			druPerfshires = cruscalAlg.druPerfshiresMinimal( Grafi, Grafi.length );
-		}
-		
+		if( lidhshmeria ){ druPerfshires = cruscalAlg.druPerfshiresMinimal( Grafi, Grafi.length ); }
 		for( int i = 0 ; i < Grafi.length ; i++ ) {
 			for( int j = i ; j < Grafi.length ; j++ ) {
-				
 				System.out.println( " Ne Drurin Perfshires ne pozicionin e : " + i + " - " + j + " eshte elementi  -------> " + druPerfshires[i][j]  );
-			}
-			
-		}
-		
+			}	
+		}	
 	}
-
-	
-
 }
